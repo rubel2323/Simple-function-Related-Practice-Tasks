15aa508014c745c6ab9b30aca81ff7b1e32a819f
Rubel <mr.adcadre41@gmail.com>
2026-02-26 09:52:17 +0600

    first commit

---
+++ task-1.js
@@ -0,0 +1,9 @@
+//Take four parameters. Multiply the four numbers and then return the result
+
+function multiplyFunc(n1, n2, n3, n4) {
+  const n = n1 * n2 * n3 * n4;
+  return n;
+}
+
+const result = multiplyFunc(2, 3, 4, 5);
+console.log(result);

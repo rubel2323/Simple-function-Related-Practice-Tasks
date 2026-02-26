72215f149061f0b25a353cd9cc271c71978aec9b
Rubel <mr.adcadre41@gmail.com>
2026-02-26 16:11:42 +0600

    zero counter added

---
+++ task-4.js
@@ -0,0 +1,9 @@
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+function count_zero(zeros) {
+  const zero = zeros.split("0").length - 1;
+  return zero;
+}
+
+const digits = count_zero("10101100");
+console.log("The total zeros are -", digits);

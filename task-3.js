6ed2e878db0a9a1152b0cc467f3a9b13b7e0d189
Rubel <mr.adcadre41@gmail.com>
2026-02-26 11:24:20 +0600

    task -3 completed

---
+++ task-3.js
@@ -0,0 +1,16 @@
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+function make_avg(arr) {
+  let array = 0;
+  for (let ar of arr) {
+    array = array + ar;
+  }
+
+  let arrSize = arr.length;
+
+  let arraySum = array / arrSize;
+  return arraySum;
+}
+const number = [2, 4, 5];
+const arrayDigits = make_avg(number);
+console.log("The digits is ", number, "and avg of digits is", arrayDigits);
